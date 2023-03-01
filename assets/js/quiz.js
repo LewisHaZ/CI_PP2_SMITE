e797ddd59c34d5988b160ec04b4da8164ea528be
LewisHaZ <feldman.lewis@Gmail.com>
2023-03-01 20:47:30 +0000

    add a function to check the answer given against the correct answer and give feedback to the user (feature)

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,18 +1,20 @@
 // https://opentdb.com/api.php?amount=10&category=20&difficulty=medium&type=multiple
 
-const question = document.getElementById('question');
-const options = document.querySelector('.quiz-questions');
+const _question = document.getElementById('question');
+const _options = document.querySelector('.quiz-questions');
 const _correctScore = document.getElementById('correct-score');
 const _totalQuestion = document.getElementById('total-question');
-const checkBtn = document.getElementById('check-answer');
-const playAgainBtn = document.getElementById('play-again');
+const _checkBtn = document.getElementById('check-answer');
+const _playAgainBtn = document.getElementById('play-again');
+const _result = document.getElementById('result');
 
 let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10;
 
 //event listeners
 function eventListeners(){
-    checkBtn.addEventListener('click', checkAnswer);
+    _checkBtn.addEventListener('click', checkAnswer);
 }
+
 document.addEventListener('DOMContentLoaded', () =>{
     loadQuestion();
     eventListeners();
@@ -25,37 +27,56 @@ async function loadQuestion(){
     const result = await fetch(`${APIUrl}`);
     const data = await result.json();
     //console.log(data.results[0]);
-    showQuestion(data.results[0])
+    showQuestion(data.results[0]);
 }
 
 // load the questions and their options
 function showQuestion(data){
-    lcorrectAnswer = data.correct_answer;
+    correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
     // inserting correct answer in a random position in the options list
     optionsList.splice(Math.floor(Math.random() * 
     (incorrectAnswer.length + 1)) , 0, correctAnswer); 
 
-    question.innerHTML = `${data.question} <br> <span class ="category">${data.category}</span>`;
-    options.innerHTML = `${optionsList.map((option, index) => `
-        <li>  ${index + 1}. <span> ${option}</span></li>`).join('')}`;
+    _question.innerHTML = `${data.question} <br> <span class ="category">${data.category}</span>`;
+    _options.innerHTML = `${optionsList.map((option, index) => `
+        <li>  ${index + 1}. <span> ${option} </span> </li>
+        `).join('')}
+        `;
 
         selectOption();
 }
 
 // options selector
 function selectOption(){
-    options.querySelectorAll('li').forEach((option) =>
-        option.addEventListener('click', () =>{
-            if(options.querySelector('.selected')){
-                const activeOption = options.querySelector('.selected');
+    _options.querySelectorAll('li').forEach((option) => {
+        option.addEventListener('click', () => {
+            if(_options.querySelector('.selected')){
+                const activeOption = _options.querySelector('.selected');
                 activeOption.classList.remove('selected');
             }
             option.classList.add('selected');
-        }))
+        });
+    });
+    console.log(correctAnswer);
 }
 
 function checkAnswer(){
-    checkBtn.disabled = true;
+    _checkBtn.disabled = true;
+    if(_options.querySelector('.selected')){
+        let selectedAnswer = _options.querySelector('.selected span').textContent;
+        if(selectedAnswer.trim() == HTMLDecode(correctAnswer)){
+            correctScore++;
+            _result.innerHTML = `<p> <i class = "fas fa-check"></i>Correct Answer! </p>`;
+        } else {
+            _result.innerHTML = `<p> <i class = "fas fa-times"></i>Incorrect Answer! </p>
+             <p> <small><b>Correct Answer: </b> ${correctAnswer}</small></p>`;
+        }
+    }
+}
+
+function HTMLDecode(textString){
+    let doc = new DOMParser().parseFromString(textString, "text/html");
+    return doc.documentElement.textContent;
 }
