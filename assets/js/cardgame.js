f5f1868a8d6061fd47202c1d0c2da314972b3fa6
LewisHaZ <feldman.lewis@Gmail.com>
2023-03-07 20:40:23 +0000

    add comments to card game js file (fix)

--- assets/js/cardgame.js
+++ assets/js/cardgame.js
@@ -24,17 +24,19 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
   // Create variable for turns counter, start it at zero
   let turns = 0;
 
+  // Get the rating id and the crown class
   const crown = document.getElementById("rating").querySelectorAll(".crown");
-
+  // set the default rating count to 3
   let ratingCount = 3;
-
+  // Timer class
   const timeCounter = document.querySelector(".timer");
-
+  // Set time to zero
   let time = 0;
   let minutes = 0;
   let seconds = 0;
   let timeStart = false;
 
+  // Shuffle the cards into a random order on the game board
   function shuffle(array){
     let currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -49,8 +51,10 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
     return array;
   }
 
+  // Start game function
   function startGame(){
     const shuffledDeck = shuffle(deckOfCards);
+    // Assigning each image to a card slot, twice for each image
     for(let i = 0; i < shuffledDeck.length; i++){
         const liTag = document.createElement('LI');
         liTag.classList.add('card');
@@ -61,18 +65,22 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
         cards.appendChild(liTag);
     }
   }
-
+  // Start the game
   startGame();
 
+  // Remove card function 
   function removeCard(){
     while (cards.hasChildNodes()){
         cards.removeChild(cards.firstChild);
     }
   }
+  // Timer function 
   function timer(){
     time = setInterval(function(){
+        // Increment the seconds by 1
         seconds++;
         if(seconds === 60){
+            // Increment the minutes by 1 every 60 seconds
             minutes ++;
             seconds = 0;
         }
@@ -81,9 +89,11 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
     }, 1000);
   }
 
+  // Stop timer function when user has finished game
   function stopTime(){
     clearInterval(time);
   }
+  // Reset the board to their default stats
   function resetEverything(){
     stopTime();
     timeStart = false;
@@ -91,24 +101,25 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
     minutes = 0;
     timeCounter.innerHTML = "<i class='fa-solid fa-hourglass-half'></i>" + 
     "Time elapsed: 00:00";
-  
-
-  crown[1].firstElementChild.classList.add("fa-crown");
-  crown[2].firstElementChild.classList.add("fa-crown");
-  ratingCount = 3;
-  turns = 0;
-  turnsCount.innerHTML = 0;
-  turned = [];
-  matched = [];
-  removeCard();
-  startGame();
+    crown[1].firstElementChild.classList.add("fa-crown");
+    crown[2].firstElementChild.classList.add("fa-crown");
+    ratingCount = 3;
+    turns = 0;
+    turnsCount.innerHTML = 0;
+     turned = [];
+    matched = [];
+    removeCard();
+    startGame();
   }
 
+  // For every two cards turned, increment turns count by 1 
   function turnsCounter(){
     turnsCount.innerHTML ++;
     turns ++;
   }
 
+  // If the user goes over 14 turns, remove a crown
+  // If the user goes over 19 turns, remove another crown
   function crownRating(){
     if(turns === 15){
         crown[2].firstElementChild.classList.remove("fa-crown");
@@ -120,18 +131,22 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
     }
   }
 
+  // Comparing two cards function
   function compareTurnedCards(){
     if(turned.length === 2){
+        // Disable extra click events when two cards are turned
         document.body.style.pointerEvents = "none";
     }
-
+    // Matched cards if statement
     if(turned.length === 2 && turned[0].src === turned[1].src){
         match();
+    // Non-matched cards
     } else if (turned.length === 2 && turned[0].src != turned[1].src){
         noMatch();
     }
   }
 
+  // If match function
   function match(){
     setTimeout(function(){
         turned[0].parentElement.classList.add("match");
@@ -145,6 +160,7 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
     crownRating();
   }
 
+  // If no match function
   function noMatch(){
     setTimeout(function(){
         turned[0].parentElement.classList.remove("flip");
@@ -156,6 +172,7 @@ const deckOfCards = ["ah-puch.png", "ah-puch.png", "anhur.png", "anhur.png",
     crownRating();
 }
 
+// Displaying the end game statistics to a modal
 function AddStats(){
     const stats = document.querySelector(".modal-content");
     for(let i = 1; i <= 3; i++){
@@ -170,6 +187,7 @@ function AddStats(){
     p[2].innerHTML = "Your Crown Rating is: " + ratingCount + "out of 3";
 }
 
+// Calls the modal to be displayed 
 function displayModal(){
     const modalClose = document.getElementsByClassName("close")[0];
     modal.style.display = "block";
@@ -183,6 +201,7 @@ function displayModal(){
     };
 }
 
+// Ends the game when all cards are matched
 function winGame(){
     if(matched.length === 16){
         stopTime();
@@ -191,6 +210,7 @@ function winGame(){
     }
 }
 
+// Checks for cards turned and starts timer
 cards.addEventListener("click", function(evt){
     if(evt.target.nodeName === "LI"){
         console.log(evt.target.nodeName + 'was clicked');
@@ -201,10 +221,12 @@ cards.addEventListener("click", function(evt){
         }
         flipCard();
     }
+    // stores the first card flip in a function
     function flipCard(){
         evt.target.classList.add("flip");
         addToTurned();
     }
+    // stores the second card flip and compares to first
     function addToTurned(){
         if(turned.length === 0 || turned.length === 1){
             turned.push(evt.target.firstElementChild);
@@ -213,6 +235,7 @@ cards.addEventListener("click", function(evt){
     }
 });
 
+// Listener for play again button click event
 reset.addEventListener('click', resetEverything);
 playAgain.addEventListener('click',function(){
     modal.style.display = "none";
