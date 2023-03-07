e81a50de4e1d7d509ca7187441f337d356a55e65
LewisHaZ <feldman.lewis@Gmail.com>
2023-03-07 20:42:17 +0000

    add comments to email js file (fix)

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,17 @@
 // Credit to Code with Voran for this code @ https://www.youtube.com/watch?v=x7Ewtay0Q78
+// Function to send email using information taken from my email.js template
 function sendMail(params){
     var tempParams = {
         from_name: document.getElementById("fromName").value,
         email: document.getElementById("email").value,
         message: document.getElementById("msg").value,
     };
-
+// Sends email to service the selected (gmail) and my personal email account for review of feedback
 emailjs
     .send('service_cqu9ojp', 'template_5hokv65',tempParams)
     .then(function(res){
         console.log("success", res.status);
+        //prints to console if it worked
     })
 }
 
